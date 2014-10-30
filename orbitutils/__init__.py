d2654d497dd10ae5554ab8b33cdca066d28a15dd
Timothy Morton <tim.morton@gmail.com>
2014-10-30 01:20:51 -0400

    reorganized, added notebooks

--- orbitutils/__init__.py
+++ orbitutils/__init__.py
@@ -1,2 +1,5 @@
-__all__ = ['orbitutils','kepler']
+__all__ = ['populations','kepler']
 
+from populations import OrbitPopulation,OrbitPopulation_FromH5
+from populations import TripleOrbitPopulation,TripleOrbitPopulation_FromH5
+from populations import BinaryGrid
