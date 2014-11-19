aaed83c032d49117e08ccd4b2e28216c60b35b96
timothydmorton <tim.morton@gmail.com>
2014-11-19 14:51:07 -0500

    Update __init__.py

--- orbitutils/__init__.py
+++ orbitutils/__init__.py
@@ -5,6 +5,8 @@ try:
 except NameError:
   __ORBITUTILS_SETUP__ = False
 
+
+if not __ORBITUTILS_SETUP__:
   __all__ = ['OrbitPopulation','OrbitPopulation_FromH5','TripleOrbitPopulation',
             'TripleOrbitPopulation','TripleOrbitPopulation_FromH5','semimajor']
   
