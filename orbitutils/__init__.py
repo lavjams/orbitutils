2782294b7afd1adcc0364850a7bf202531fb8058
Timothy Morton <tim.morton@gmail.com>
2015-01-27 14:45:54 -0500

    added imports in __init__

--- orbitutils/__init__.py
+++ orbitutils/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 try:
   __ORBITUTILS_SETUP__
@@ -7,11 +7,14 @@ except NameError:
 
 
 if not __ORBITUTILS_SETUP__:
-  __all__ = ['OrbitPopulation','OrbitPopulation_FromH5','TripleOrbitPopulation',
-            'TripleOrbitPopulation','TripleOrbitPopulation_FromH5','semimajor']
+  __all__ = ['OrbitPopulation','OrbitPopulation_FromH5',
+             'OrbitPopulation_FromDF','TripleOrbitPopulation',
+            'TripleOrbitPopulation_FromDF','TripleOrbitPopulation_FromH5',
+             'semimajor']
   
   from .populations import OrbitPopulation,OrbitPopulation_FromH5
   from .populations import TripleOrbitPopulation,TripleOrbitPopulation_FromH5
+  from .populations import OrbitPopulation_FromDF,TripleOrbitPopulation_FromDF
   from .populations import BinaryGrid
   
   from .utils import semimajor
